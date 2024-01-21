a3f8c68342c1dfc74746123561f24b93a675e1cd
Reda Zahri <146728013+Ressim16@users.noreply.github.com>
2024-01-21 14:47:27 +0100

    Uploading Step1
    all html, sbatch and response to 1st question

---
+++ Step1/step1_all_fatsqc.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=20G
+#SBATCH --time=04:00:00
+#SBATCH --job-name=rzahri
+#SBATCH --mail-user=reda.zahri@unifr.ch
+#SBATCH --mail-type=fail
+#SBATCH --job-name="All_3FastQC"
+#SBATCH --output=/data/courses/rnaseq_course/lncRNAs/Project2/users/rzahri/step1/output_slurm_FASTQC-%j.o
+#SBATCH --error=/data/courses/rnaseq_course/lncRNAs/Project2/users/rzahri/step1/error_slurm_FASTQC-%j.e
+
+
+###moving to the right directoy
+cd /data/courses/rnaseq_course/lncRNAs/Project2/users/rzahri/step1
+
+###loading module
+module load UHTS/Quality_control/fastqc/0.11.9
+
+###Paraclonal cells
+for file in /data/courses/rnaseq_course/lncRNAs/fastq/3*.fastq.gz
+do 
+    fastqc $file
+done
+
+###Parental cells
+for file in /data/courses/rnaseq_course/lncRNAs/fastq/P*.fastq.gz
+do 
+    fastqc $file
+done
+
+### The Fastqc Sofwtare allows us to analyze the quality of our reads
+###downloading the _fastqc.html to check for the data quality
+# scp rzahri@login8.hpc.binf.unibe.ch:/data/courses/rnaseq_course/lncRNAs/Project2/users/rzahri/step1/*.html C:\Users\redaz\Documents\Uni\Master\RNA-seq\Steps\Step1\
+
+###The volume and amount of data are showed with the help of bash/awk
+touch Volume_of_data.txt
+echo -e "###Number of lines per file :" > Volume_of_data.txt
+
+for file in /data/courses/rnaseq_course/lncRNAs/fastq/[P3]*.fastq.gz
+do 
+    wc -l $file >> Volume_of_data.txt
+done
+
+echo -e "\n###Volume of data [GB] per file :" >> Volume_of_data.txt
+for file in /data/courses/rnaseq_course/lncRNAs/fastq/[P3]*.fastq.gz
+do
+    ls -sh $file >> Volume_of_data.txt
+done
+
+echo -e "\n###Volume and ratio of zipped and decompressed file:" >> Volume_of_data.txt
+for file in /data/courses/rnaseq_course/lncRNAs/fastq/[P3]*.fastq.gz
+do
+    gunzip -l $file >> Volume_of_data.txt
+done
+
