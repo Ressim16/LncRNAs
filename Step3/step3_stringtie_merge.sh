080637cadc22258e6db6b65a61bc119f45f64f58
Reda Zahri <146728013+Ressim16@users.noreply.github.com>
2024-01-23 14:12:34 +0100

    Adding Gene_Type to target_mapping

--- Step3/step3_stringtie_merge.sh
+++ Step3/step3_stringtie_merge.sh
@@ -46,4 +46,10 @@ stringtie --merge -p 4 -G $GEN_ANN -o ALL_cells_merged.gtf Transcripts_ALL_GTF.t
 # $'\t' is for the separator type, 
 #so here we choose only the transcripts that have been already annotated (with the gene_name) 
 #and we select their transcripts ID (1st column of target mapping output) and their gene_name (2nd column of the target mapping output)
-awk -F $'\t' '$3=="transcript"' ALL_cells_merged.gtf | grep 'gene_name' | awk -F $'\t' '{print $9}' | awk -F ';' '{print $2, $3}' | awk '{print $2, $4}' | sed 's/"//g' > target_mapping.txt
+awk -F $'\t' '$3=="transcript"' ALL_cells_merged.gtf | grep 'gene_name' | awk -F $'\t' '{print $9}' | awk -F ';' '{print $2, $3}' | awk '{print $2, $4}' | sed 's/"//g' > TranscriptID_GeneName.txt
+#selecting gene_type from gencode.v44.primary_assembly.annotation.gtf
+awk -F $'\t' '{print $9}' gencode.v44.primary_assembly.annotation.gtf | awk -F ';' '$2 ~ "transcript_id" {print $2 $3}' | awk '{print $2 $4}' | sed 's/"/ /g' > TranscriptID_GeneType.txt
+#joining the gene_type to the target mapping : sort first and join them in bash
+sort TranscriptID_GeneName.txt > TranscriptID_GeneName.sorted
+sort TranscriptID_GeneType.txt > TranscriptID_GeneType.sorted
+join TranscriptID_GeneName.sorted TranscriptID_GeneType.sorted > target_mapping.txt
