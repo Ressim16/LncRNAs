8963fc8cdb1245e08acb1fabe9f48bbc16c0012e
Reda Zahri <146728013+Ressim16@users.noreply.github.com>
2024-01-28 17:12:31 +0100

    Ameliorate plots aestetics

--- Step5/Step5_LncRNAs.R
+++ Step5/Step5_LncRNAs.R
@@ -65,8 +65,8 @@ sleuth_significant <- dplyr::filter(sleuth_table, qval <= 0.05)
 # taking care with significance cutoffs (here 0.05 we use)
 sleuth_significant_Novel<-sleuth_significant[is.na(sleuth_significant$gene_type),]
 sleuth_significant_Known<-sleuth_significant[!is.na(sleuth_significant$gene_type),]
-write.csv(sleuth_significant_Novel, file="Novel_Significan_DE_Genes", row.names = F, quote = F)
-write.csv(sleuth_significant_Known, file="Known_Significan_DE_Genes", row.names = F, quote = F)
+write.csv(sleuth_significant_Novel, file="Novel_Significant_DE_Genes.csv", row.names = F, quote = F)
+write.csv(sleuth_significant_Known, file="Known_Significant_DE_Genes.csv", row.names = F, quote = F)
 
 
 
@@ -82,14 +82,39 @@ EnhancedVolcano(sleuth_table_Novel,
                 lab=sleuth_table_Novel$target_id,
                 x="b",
                 y="qval")
-#for only known genes and transcripts
+#for only known genes and transcripts (I developed the aestetics for the report)
 EnhancedVolcano(sleuth_table_Known,
                 lab=sleuth_table_Known$gene_name,
                 x="b",
-                y="qval")
+                y="qval",
+                title="DE of Annotated Genes between 2 conditions",
+                pCutoff = 0.001788080,
+                FCcutoff = 0.5,
+                labSize=3.0,
+                col=c("grey", "blue", "magenta", "red"),
+                colAlpha = 1,
+                legendLabels=c('Not sig.','Log (base 2) FC','p-value',
+                               'p-value & Log (base 2) FC'),
+                legendLabSize = 10,
+                legendIconSize = 5.0,
+                drawConnectors = TRUE,
+                widthConnectors = 0.5,
+                colConnectors = "grey40",
+                max.overlaps = 20
+                ) + coord_flip()
 
 #Creating boxplots and taking one of the 20 genes and replace it in the 2nd argument below
-plot_bootstrap(so, "MSTRG.9599.5", units = "est_counts", color_by = "condition")
+# (I developed the aestetics for the report)
+sleuth_significant_Known[sleuth_significant_Known$gene_name=="IL6",] #taking the ENST00000407492.2 target ID
+plot_bootstrap(so, "ENST00000464710.2", units = "est_counts",
+               color_by = "condition",
+               x_axis_angle = 50)+
+  scale_fill_manual(values=c("darkturquoise", "darkorchid"),
+                    labels=c("Parental", "Paraclonal")) +
+  ggtitle("Estimated counts for the IL6 gene")+
+  ylab("Estimated counts")+
+  xlab("Cell types")
+
 #Creating PCA plot
 plot_pca(so, color_by = 'condition')
 #quality control to check the amount of counts per conditions --> should be the same
@@ -101,7 +126,7 @@ plot_group_density(so, use_filtered = TRUE, units = "est_counts",
 #we can analyse the volcano plot for known genes and 
 # check if the genes are differntially expressed in lung cancer 
 # e.g CXCL8 == ENST00000307407.8 (upregulated) and MYO10 == ENST00000513610.6 (downregulated) 
-# also EGFR or MALAT1 that are already well known in cancer implicaitons
+# also EGFR or MALAT1 or IL6 that are already well known in cancer implicaitons
 
 
 
